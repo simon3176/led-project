280b8e68605db35d77d41dfd163c74a3d98b4235
Nomis3167 <113910266+simon3176@users.noreply.github.com>
2022-10-04 09:13:26 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showIcon(IconNames.Ghost)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(1000)
+})
